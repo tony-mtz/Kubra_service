e7937898afe97e1af9d9b501508d58274e57fd8f
tony <anthony@amartinez.cc>
2021-03-23 12:37:07 -0700

    base server

--- server/server.js
+++ server/server.js
@@ -1,54 +1,42 @@
-const path = require('path');
 const express = require('express');
-// const cookieParser = require('cookie-parser');
 const {ApolloServer, gql} = require('apollo-server-express')
+const {RESTDataSource} = require('apollo-datasource-rest')
 
-// const {typeDefs} = require('./typeDefs')
-// const {resolvers} = require('./resolvers')
-const { PrometheusAPI } = require('./dataSource');
-// const app = express();
 const PORT = 9000;
-/**
- * parse request body
- */
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(cookieParser());
 
-/**
- * handle static files
- */
-// app.use(express.static(path.resolve(__dirname, '../build')));
+const API_URL = 'http://104.200.26.218:8080/api/v1/';
+
+class PrometheusAPI extends RESTDataSource{
+  constructor(){
+    super()
+    this.baseURL = API_URL;
+  }
+
+  async getCpuUsageSecondsTotal(){
+   
+    const data = await this.get('query_range?query=sum(rate(container_cpu_usage_seconds_total{id="/"}[1m]))/sum((machine_cpu_cores)*100)&start=2021-03-19T19:12:52.00Z&end=2021-03-21T20:12:52.00Z&step=1m')
+   //can fix data here to conver or just get correct data
+   console.log(data.status)
+
+   return data;
+  }
+}
 
 const typeDefs = gql`
-
-  type Value{
-    date: Int! 
-    value: String!
-  }
-
-  type Values{
-    Value: [Value]
-  }
-
   type Data{
-    resultType: String
-    result:[Values]
-
-  }
-  type ResultObject{
-    status: String!
-    data: Data 
-  }
+    status : String
+  }  
 
   type Query{
-    getCpuUsageSecondsTotal(): Data
+    cpuUsage: Data
+    
   }
 `
+
 const resolvers = {
   Query:{
-    async cpuUsage(_, _args, {dataSources}){
-      dataSources.prometheusAPI.getCpuUsageSecondsTotal()
+    cpuUsage: async(_, _args, {dataSources})=>{
+      return dataSources.prometheusAPI.getCpuUsageSecondsTotal()
     }
   }
 }
